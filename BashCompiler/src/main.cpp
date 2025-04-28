d7c6f50e5a356ef64cd043b9e874d985a9052c93
keithhedger <keithhedger@bdc30d43-1fda-46b6-80d7-e06374c2f371>
2025-04-28 15:26:01 +0000

    done if
    git-svn-id: file:///media/LinuxData/Development64/SVN/BashCompiler@2 bdc30d43-1fda-46b6-80d7-e06374c2f371

--- BashCompiler/src/main.cpp
+++ BashCompiler/src/main.cpp
@@ -1,15 +1,61 @@
+/*
+ *
+ * ©K. D. Hedger. Fri 18 Apr 20:31:32 BST 2025 keithdhedger@gmail.com
 
+ * This file (main.cpp) is part of BashCompiler.
 
-#include <QApplication>
-#include <QWidget>
-#include <QMessageBox>
+ * BashCompiler is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
 
-int main(int argv, char **args)
+ * BashCompiler is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with BashCompiler.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "globals.h"
+
+parseFileClass	*mainParseClass=NULL;
+commandsClass	*mainCommandsClass=NULL;
+
+int main(int argc,char **argv)
 {
-	QApplication	app(argv, args);
-	QMessageBox		msgBox;
+	QApplication	app(argc,argv);
 
-	msgBox.setText("Hello World!");
-	msgBox.show();
-	return app.exec();
+	mainParseClass=new parseFileClass(argv[1]);
+	mainCommandsClass=new commandsClass();
+
+	mainParseClass->verboseCompile=true;
+	mainParseClass->verboseCCode=false;
+	mainParseClass->parseFile();
+
+	mainParseClass->cFileDeclares.removeDuplicates();
+
+	QString functions="std::string procsub(std::string proc)\n\
+{\n\
+FILE *fp;\n\
+char *buffer=(char*)alloca(1024);\n\
+std::string retstr=\"\";\n\
+fp=popen(proc.c_str(),\"r\");\n\
+if(fp!=NULL)\n\
+{\n\
+buffer[0]=0;\n\
+while(fgets(buffer,1024,fp))\n\
+retstr+=buffer;\n\
+retstr.erase(retstr.length()-1,1);\n\
+pclose(fp);\n\
+}\n\
+return(retstr);\n\
+}\n";
+
+	printf("//C File\n#include <string>\n#include <cstdio>\nbool inQuotes=false;\n%s\n%s\nint main(int argc,char **argv)\n{\n%s\n}\n",functions.toStdString().c_str(),mainParseClass->cFileDeclares.join("\n").toStdString().c_str(),mainParseClass->cFile.toStdString().c_str());
+
+	delete mainParseClass;
+	delete mainCommandsClass;
+	return(0);
 }
